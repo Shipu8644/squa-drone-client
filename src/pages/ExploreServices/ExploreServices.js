7a5f5950086fdff8011db3a98a4d3ec2a9f638e7
Shipu8644 <shipu.monjurur@gmail.com>
2021-11-11 10:12:41 +0600

    spinner added

--- src/pages/ExploreServices/ExploreServices.js
+++ src/pages/ExploreServices/ExploreServices.js
@@ -2,7 +2,7 @@ import { Container, Grid, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import ExploreService from '../ExploreService/ExploreService';
 import Navbar from '../Shared/Navigation/Navbar/Navbar';
-
+import CircularProgress from '@mui/material/CircularProgress';
 const ExploreServices = () => {
     const [services, setServices] = useState([]);
 
@@ -14,7 +14,7 @@ const ExploreServices = () => {
     return (
         <div>
             <Navbar></Navbar>
-            <Container sx={{ flexGrow: 1, my: 3 }}>
+            {services.length ? <Container sx={{ flexGrow: 1, my: 3 }}>
                 <Typography sx={{ my: 3, fontWeight: 'bold' }} variant='h4'> Services</Typography>
                 <Grid container spacing={3}>
                     {services.map(service => <ExploreService
@@ -22,6 +22,9 @@ const ExploreServices = () => {
                     ></ExploreService>)}
                 </Grid>
             </Container>
+                :
+                <CircularProgress />
+            }
         </div>
     );
 };
